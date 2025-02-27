89e18bbf200bf1f0763d4112721d41af557c346d
Marcelo A. Torres Alegría <martoral@gmail.com>
2025-02-27 09:23:05 -0300

    Actualización Segundo Desafío

--- app.js
+++ app.js
@@ -22,3 +22,18 @@ if(numeroDiez == stringDiez){
 } else {
     console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
 }
+
+//¿Sabes cuando te registras en un site y, justo después, al iniciar sesión, te llama por tu nombre? Eso es lo que harás en el desafío de hoy.
+// Desarrollar un programa simulando una de esas aplicaciones. Debe pedir al usuario responder 3 preguntas:
+// - ¿Cuál es tu nombre?
+//- ¿Cuántos años tienes?
+//- ¿Qué lenguaje de programación estás estudiando?
+//A medida que se hagan las preguntas, la persona que esté usando el programa debe responder cada una de ellas.
+//Al final, el sistema mostrará el mensaje:
+//"Hola [nombre], tienes [edad] años y ya estás aprendiendo [lenguaje]!"
+
+let nombre = prompt('¿Cuál es tu nombre?');
+let edad = prompt('¿Cuántos años tienes?');
+let lenguajeProgramación = prompt('¿Qué lenguaje de programación estás estudiando?');
+
+console.log(`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguajeProgramación}`);
